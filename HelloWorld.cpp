23c4ddc49ae4528aa8e878a2fbb1bc0d4ffc5c95
Jay <hr0335@wayne.edu>
2025-09-13 23:12:18 -0400

    adding Authour Name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+//Authour : Jay Tieking <Hr0335@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
